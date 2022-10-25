ddb713099d1e4068dbeabc2a5b0dee566db6b6c5
PaidUser1 <116077835+PaidUser1@users.noreply.github.com>
2022-10-25 18:20:45 +0500

    Update A.py

--- A.py
+++ A.py
@@ -1,30 +1,17 @@
-import os, platform, time
-os.system('termux-setup-storage')
+import os, platform
 try:
-	import requests
-except ImportError:
-	print('\n [×] requests module not installed!...\n')
-	os.system('pip install requests')
-
-try:
-	import concurrent.futures
-except ImportError:
-	print('\n [×] Futures module not installed!...\n')
-	os.system('pip install futures')  
-try:
-	import bs4
-except ImportError:
-	print('\n [×] Bs4 module not installed!...\n')
-	os.system('pip install bs4')
-print('\n\x1b[1;37m[•] Checking Update...');time.sleep(0.5)
-os.system('git pull')
-def Run():
-        bit = platform.architecture()[0]
-        if bit == '32':
-            print("\x1b[1;92m[•] Congratulations ! Your Device Support this Tools")
-            print('[•] Join Over Facebook Group First')
-            os.system('xdg-open https://facebook.com/groups/351076900316263/')
-            import AKING
-        else:
-            exit('\033[1;31m[×] Sorry, Your Device Not Support This Tool')
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from data import main
+    main()
+elif bit == '32bit':
+    from data32 import main
+    main()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
 Run()
